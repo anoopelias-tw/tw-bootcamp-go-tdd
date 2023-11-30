b5c4b6591843fa79f985a9659c005179b3b35d42
Anoop Elias <Anoop.Elias@thoughtworks.com>
2023-11-30 15:02:07 +0530

    Calculate fare for wait time as well

--- cab.go
+++ cab.go
@@ -2,12 +2,13 @@ package main
 
 type CabRide struct {
 	distance int
+	waitTime int
 }
 
-func NewCabRide(distance int) CabRide {
-	return CabRide{distance: distance}
+func NewCabRide(distance int, waitTime int) CabRide {
+	return CabRide{distance: distance, waitTime: waitTime}
 }
 
 func (r *CabRide) Fare() int {
-	return r.distance * 10
+	return r.distance*10 + r.waitTime*2
 }
--- cab_test.go
+++ cab_test.go
@@ -7,13 +7,18 @@ import (
 
 var _ = Describe("CabRide", func() {
 	It("should calculate fare as 50 when distance is 5", func() {
-		cabRide := NewCabRide(5)
+		cabRide := NewCabRide(5, 0)
 		fare := cabRide.Fare()
 		Expect(fare).To(Equal(50))
 	})
 	It("should calculate fare as 60 when distance is 6", func() {
-		cabRide := NewCabRide(6)
+		cabRide := NewCabRide(6, 0)
 		fare := cabRide.Fare()
 		Expect(fare).To(Equal(60))
 	})
+	It("should calculate fare as 6 when waiting time is 3 minutes", func() {
+		cabRide := NewCabRide(0, 3)
+		Expect(cabRide.Fare()).To(Equal(6))
+	})
+
 })
